f6056df1dff617524634131f6f53c101bc34ae6a
4meta5 <asinghchrony@protonmail.com>
2021-08-05 19:38:57 +0200

    minimal viable solution

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::fs::{self, File};
+use std::io::Write;
+
+fn main() {
+    fs::remove_dir_all("test/src").ok();
+    fs::remove_dir_all("test").ok();
+    fs::create_dir_all("test").expect("failed to create test/ directory");
+    fs::create_dir_all("test/src").expect("failed to create test/src directory");
+    let cargo_toml_txt = format!(
+        r#"[package]
+name = "test"
+version = "0.1.0"
+authors = ["Amar Singh"]
+edition = "2018"
+
+[dependencies]
+
+[workspace]
+		"#,
+    );
+    let mut file = File::create("test/Cargo.toml").expect("failed to create file");
+    file.write_all(cargo_toml_txt.as_bytes())
+        .expect("failed to write data to share file");
+    let mut file = File::create("test/src/main.rs").expect("failed to create file");
+    let code = format!(
+        r#"
+fn main() {{ println!("HELLO WORLD"); }}
+        "#,
+    );
+    file.write_all(code.as_bytes())
+        .expect("failed to write data to share file");
+}
