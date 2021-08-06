c5d880333e7a7e92adc39b4831aa29c6afcff1a2
4meta5 <asinghchrony@protonmail.com>
2021-08-06 20:41:22 +0200

    infinite writer loop

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rust Writes Rust
+
+This program writes itself into a subdirectory and then does `cargo run` in that subdirectory. You can guess what happens next.
+
+> *If you ever do `cargo run` this program, `CTRL-C` to stop the infinite code generation. You can delete all generated code by removing the root-level `test/` directory.*
--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,45 @@ use std::fs::{self, File};
 use std::io::Write;
 
 fn main() {
-    fs::remove_dir_all("test/src").ok();
-    fs::remove_dir_all("test").ok();
-    fs::create_dir_all("test").expect("failed to create test/ directory");
-    fs::create_dir_all("test/src").expect("failed to create test/src directory");
+    // Paths
+    let directory = "test";
+    let code_subdirectory = format!("{}/src", directory);
+    let cargo_toml_path = format!("{}/Cargo.toml", directory);
+    let code_file_path = format!("{}/main.rs", code_subdirectory);
+    // Clean existing directory if they exist
+    fs::remove_dir_all(code_subdirectory.clone()).ok();
+    fs::remove_dir_all(directory).ok();
+    // Create folders
+    fs::create_dir_all(directory).expect("failed to create test/ directory");
+    fs::create_dir_all(code_subdirectory).expect("failed to create test/src directory");
+    // Cargo toml text
     let cargo_toml_txt = format!(
         r#"[package]
-name = "test"
+name = "{}"
 version = "0.1.0"
-authors = ["Amar Singh"]
+authors = ["Parent Directory"]
 edition = "2018"
 
 [dependencies]
-
-[workspace]
-		"#,
-    );
-    let mut file = File::create("test/Cargo.toml").expect("failed to create file");
-    file.write_all(cargo_toml_txt.as_bytes())
-        .expect("failed to write data to share file");
-    let mut file = File::create("test/src/main.rs").expect("failed to create file");
-    let code = format!(
-        r#"
-fn main() {{ println!("HELLO WORLD"); }}
         "#,
+        directory
     );
-    file.write_all(code.as_bytes())
+    // Code in this file, TODO: limit depth so not infinite
+    let code = include_str!("./main.rs");
+    // Create files
+    let mut cargo_toml_file = File::create(cargo_toml_path).expect("failed to create file");
+    let mut code_file = File::create(code_file_path).expect("failed to create file");
+    // Write to files
+    cargo_toml_file
+        .write_all(cargo_toml_txt.as_bytes())
         .expect("failed to write data to share file");
+    code_file
+        .write_all(code.as_bytes())
+        .expect("failed to write data to share file");
+    // Run cargo run command in new directory
     std::process::Command::new("cargo")
         .arg("run")
-        .current_dir("test")
+        .current_dir(directory)
         .status()
         .expect("failed to run command");
 }
